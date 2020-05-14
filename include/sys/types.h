a4b00ec41b1285140ae356b41cebdb086c53a393
Ishimoto Shinobu <nagakamira@gmail.com>
2020-05-14 12:00:28 +0000

    some
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- include/sys/types.h
+++ include/sys/types.h
@@ -169,4 +169,9 @@ typedef	__timer_t	timer_t;
 typedef	__off_t		off_t;
 #endif
 
+/* Major, minor numbers, dev_t's. */
+#define	major(x)	(((unsigned)(x) >> 8) & 0xff)
+#define	minor(x)	((unsigned)((x) & 0xff) | (((x) & 0xffff0000) >> 8))
+#define	makedev(x,y)	((dev_t)((((x) & 0xff) << 8) | ((y) & 0xff) | (((y) & 0xffff00) << 8)))
+
 #endif
